7106acc9ca4a1586bc016b7844ea979f3e952a56
Philipp Graw <philipp@atenia.ai>
2025-04-14 12:58:51 +0200

    initial commit

---
+++ target/debug/deps/cookbook-bd80b5c6dae1ffa9.d
@@ -0,0 +1,5 @@
+/Users/philippgraw/Desktop/DevProjects/PrivateProjects/cookbook/target/debug/deps/libcookbook-bd80b5c6dae1ffa9.rmeta: src/main.rs
+
+/Users/philippgraw/Desktop/DevProjects/PrivateProjects/cookbook/target/debug/deps/cookbook-bd80b5c6dae1ffa9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cookbook-dc11f274cc046acc.d
@@ -0,0 +1,5 @@
+/Users/philippgraw/Desktop/DevProjects/PrivateProjects/cookbook/target/debug/deps/libcookbook-dc11f274cc046acc.rmeta: src/main.rs
+
+/Users/philippgraw/Desktop/DevProjects/PrivateProjects/cookbook/target/debug/deps/cookbook-dc11f274cc046acc.d: src/main.rs
+
+src/main.rs:
